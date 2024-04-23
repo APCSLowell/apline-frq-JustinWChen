0e0dc2b91c19d282db4855ffeac89aea61be0bb8
JustinWChen <143028009+JustinWChen@users.noreply.github.com>
2024-04-23 10:40:07 -0700

    Update APLine.java

--- src/main/java/APLine.java
+++ src/main/java/APLine.java
@@ -2,13 +2,13 @@ public class APLine
 {
   /* your code here */
   private int a, b, c;
-  public APLine (int a1, int b1, int c1){
+  public APLine (double a1, double b1, double c1){
   a = a1;
     b = b1;
     c = c1;
   }
   public double getSLope(){
-return -(double)a/b;
+return -a/b;
   }
   public boolean isOnline(int x, int y){
  if (a*x+b*y+c == 0)
